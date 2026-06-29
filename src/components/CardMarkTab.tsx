411acafeb012a1df43dac4fc75a28278762d3ad2
wasuuib-cell <wasuuib@gmail.com>
2026-06-29 14:01:07 +0530

    Fix Google Drive image embed URL in CardMarkTab

--- src/components/CardMarkTab.tsx
+++ src/components/CardMarkTab.tsx
@@ -136,7 +136,7 @@ export const CardMarkTab: React.FC = () => {
     if (url && url.includes('drive.google.com/uc')) {
       const match = url.match(/id=([^&]+)/);
       if (match && match[1]) {
-        return `https://drive.google.com/thumbnail?id=${match[1]}&sz=w1200`;
+        return `https://drive.google.com/uc?export=view&id=${match[1]}`;
       }
     }
     return url;
